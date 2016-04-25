512ed3f11dc54aa5d1b3bc90501062b5ca146ccb
Saurav Keshari Aryal <saurav.aryal@bison.howard.edu>
2016-04-24 23:14:53 -0400

    added test class and data file

--- JavaCollectionsAssignment/src/Test/Test.java
+++ JavaCollectionsAssignment/src/Test/Test.java
@@ -13,23 +13,27 @@ public class Test {
 		
 		Polynomial polynomialObject = new Polynomial();
 		
-		String line=null, exponent_string = "", coefficient_string = "";
-		int exponent=0,coefficient=0;
-		File fileName = new File("file.txt");
+		String file_line = null, exponent_string = "", coefficient_string = "";
+		
+		int exponent = 0, coefficient = 0;
+		
+		File fileName = new File("data.txt");
 		try{
 			FileReader fileReader = new FileReader(fileName);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			int length=bufferedReader.read();
+			int length = bufferedReader.read();
+			
 			/**
-			 * This is check if the file is empty.
-			 * If it is empty, all the other operations are disqualified. 
+			 * If file is empty, console log error. 
 			 */
-			if(length==-1){
+			
+			if(length == -1){
 				System.out.println("Error!! The file is empty");
 			}
+			
 			else{
-				while((line=bufferedReader.readLine())!=null){
-					String[] spiltter=line.split(";");
+				while((file_line=bufferedReader.readLine())!=null){
+					String[] spiltter=file_line.split(";");
 					for(int i=1;i<spiltter.length;i++){
 						if(i%2!=0){
 							coefficient_string=spiltter[i];
@@ -38,40 +42,28 @@ public class Test {
 						else{
 							exponent_string=spiltter[i];
 							exponent=Integer.parseInt(exponent_string);
-							//Terms termsObject= new Terms(exponent,coefficient);
-							polynomialObject.Insert(coefficient,exponent);
-							
-							
-							//System.out.println("The exponent is : "+termsObject.get_coefficient()+ " The exponent is : "+termsObject.get_exponent()+" The exponent is : "+termsObject.toString());
+							polynomialObject.Insert(coefficient,exponent);							
 						}
-						
-						
 					}
 					
-					System.out.println("P(" + count+")"+" : "+polynomialObject.return_polynomial());
-					System.out.println("The product of the terms is : "+polynomialObject.Product());
+					System.out.println("P(" + count+")" + " : " + polynomialObject.return_polynomial());
+					System.out.println("The product of the polynomial's term is: " + polynomialObject.Product());
 					polynomialObject.Delete(7,1);
-					System.out.println("The polynomial after deleting is ");
-					System.out.println("P(" + count+")"+" : "+polynomialObject.return_polynomial());
-					System.out.println("The polynomial after reversing");
-					System.out.println("P(" + count+")"+" : "+polynomialObject.Reverse());
-					System.out.println("The product of the terms is : "+polynomialObject.Product());
+					System.out.println("After deleting (7,1) the Polynomial becomes: ");
+					System.out.println("P(" + count+")" + " : " + polynomialObject.return_polynomial());
+					System.out.println("After reversing the polynomail becomes: ");
+					System.out.println("P(" + count+")" + " : " + polynomialObject.Reverse());
+					System.out.println("The new product of the polynomial's term is: " + polynomialObject.Product() + "\n\n\n");
 					count++;
-					//polynomialObject.clearArrayList();
-					System.out.println("\n\n\n");
-					
 				}
 			}
 			bufferedReader.close();
-			
-			
 		}
 		catch(FileNotFoundException ex){
-			System.out.println("Unable to open file");
+			System.out.println("File not found!");
 		}
 		catch(IOException ex){
 			System.out.println("ERROR : "+ex);
 		}
 	}
-
 }
