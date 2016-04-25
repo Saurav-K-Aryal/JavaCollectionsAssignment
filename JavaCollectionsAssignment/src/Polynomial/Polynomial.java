1a3ed5cb8c96161d02854654b6f243beaedd5cf2
Saurav Keshari Aryal <saurav.aryal@bison.howard.edu>
2016-04-24 21:37:02 -0400

    Added class definition

--- JavaCollectionsAssignment/src/Polynomial/Polynomial.java
+++ JavaCollectionsAssignment/src/Polynomial/Polynomial.java
@@ -3,3 +3,15 @@ import Terms.Terms;
 import java.util.*;
 import java.io.*;
 
+/**
+ * This class will have 5 member functions.
+ * 1) Constructor: Initializes the class with the arrayList.
+ * 2) Insert: Inserts the exponents and the coefficients.
+ * 3) return_polynomial: Returns the polynomial in asceding order.
+ * 4) Delete: Removes the term with passed coefficients and exponents.
+ * 5) Reverse: Reverses the polynomial.
+ * 6) Product: Computes the product of all the terms.
+ * 7) Clear: Clears the array list.
+ * @author Sudheesh
+ *
+ */
